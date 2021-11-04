7b018de8538ede106d6826a286fd6fb634e2d15a
xxxzoro <93711548+xxxzoro@users.noreply.github.com>
2021-11-04 23:17:28 +0700

    Add files via upload

---
+++ NekoRobot/modules/musicplayer.py
@@ -0,0 +1,20 @@
+__mod_name__ = "Music Play"
+
+__help__ = """
+=> *Song Playing* 🎶
+❍ /play <song name> - play song you requested
+❍ /song <song name> - download song you requestes via youtube
+❍ /vsong <song name> - download video you requested via youtube
+❍ /search <query> - search videos on youtube with details
+=> *Admins only*
+❍ /player - open music player settings panel
+❍ /pause - pause song play
+❍ /resume - resume song play
+❍ /skip - play next song
+❍ /end - stop music play
+❍ /userbotjoin - invite assistant to your chat
+❍ /admincache - Refresh admin list
+=>>   *Use*
+1) Add @Asisstanceneko in your group to admin with all permission
+2) Add Assistant I'd to your group (coming)
+"""
